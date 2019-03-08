ceb3c5d44022d33cd689b8ac2607b0b3cd6a774a
Brian Tambara <btambara@gmail.com>
2019-03-07 16:56:42 -0800

    Init Commit

---
+++ service/src/main/java/com/service/projecttinylibrary/ProjecttinylibraryApplication.java
@@ -0,0 +1,13 @@
+package com.service.projecttinylibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecttinylibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecttinylibraryApplication.class, args);
+	}
+
+}
---
+++ service/src/test/java/com/service/projecttinylibrary/ProjecttinylibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.service.projecttinylibrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjecttinylibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
